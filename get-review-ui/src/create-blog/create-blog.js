f376b10037765c0d847ad20177f8204d031be5a0
ajinkyakamble8 <ajinkyakamble8@gmail.com>
2020-01-02 15:33:12 +0530

    Typo for writing instruction on create blog screen.

--- get-review-ui/src/create-blog/create-blog.js
+++ get-review-ui/src/create-blog/create-blog.js
@@ -47,14 +47,14 @@ function CreateBlog() {
   return (
     <div className="create-blog">
         <div className="create-blog-instructions">
-          <h3>Write A Blog To Express Your Thoughts</h3>
-          <p>Write your thoughts and get the responce from the world what they thinks on it.</p>
+          <h2>Write A Blog To Express Your Thoughts</h2>
+          <h4>Writing Instructions :</h4>
           <p><b>1. Blog Title :</b> Write the unique title that will make an impact on readers.</p>
-          <p><b>2. Written By :</b> Write your name. It will be great if you have a habit of writting your email id in this field that will help your reader to get easiely connect with you.</p>
-          <p><b>3. Content :</b> Write your thoughts with proper explaination and proofs. That will create an intrest in your reader to read the blog.</p>
-          <p><b>4. Published By :</b> Write your organization name to advertise with usefull thoughts.</p>
+          <p><b>2. Written By :</b> Write your name. It will be great if you have a habit of writing your email id in this field that will help your reader to get easily connect with you.</p>
+          <p><b>3. Content :</b> Write your thoughts with proper explanation and proofs. That will create an interest in your reader to read the blog.</p>
+          <p><b>4. Published By :</b> Write your organization name.</p>
           <p><b>5. Create :</b> Verify all the details and click on "Create" button that will publish your blog.</p>
-          <p><b>6. Cancel :</b> "Cancel" button will transfer you to on home page. It will erase all the contents you written. Please be carefull when you are clicking on "Cancel" button.</p>
+          <p><b>6. Cancel :</b> "Cancel" button will transfer you to on home page. It will erase all the contents you written. Please be careful when you are clicking on "Cancel" button.</p>
         </div>
         <div className="create-blog-card">
             <div className="form-body">
